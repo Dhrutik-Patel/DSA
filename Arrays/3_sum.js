b2ec2b385530228a3e51d877e31b60e952e72d99
Dhrutik-Patel <dhrutikpatel2017@gmail.com>
2025-06-01 09:29:06 +0530

    [IMP] Arrays: three sum problem solution

---
+++ Arrays/3_sum.js
@@ -0,0 +1,35 @@
+// Problem Statement: Given an array of N integers, your task is to find unique triplets that add up to give a sum of zero. In short, you need to return an array of all the unique triplets [arr[a], arr[b], arr[c]] such that i!=j, j!=k, k!=i, and their sum is equal to zero.
+
+// Using a two-pointer approach to find unique triplets that sum to zero.
+function threeSum(nums) {
+    const result = [];
+    nums.sort((a, b) => a - b); // Sort the array to use two-pointer technique
+
+    for (let i = 0; i < nums.length - 2; i++) {
+        if (i > 0 && nums[i] === nums[i - 1]) continue;
+
+        let left = i + 1;
+        let right = nums.length - 1;
+
+        while (left < right) {
+            const sum = nums[i] + nums[left] + nums[right];
+
+            if (sum === 0) {
+                result.push([nums[i], nums[left], nums[right]]);
+                while (left < right && nums[left] === nums[left + 1]) left++;
+                while (left < right && nums[right] === nums[right - 1]) right--;
+                left++;
+                right--;
+            } else if (sum < 0) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+    }
+
+    return result;
+}
+
+const nums = [-1, 0, 1, 2, -1, -4];
+console.log(threeSum(nums)); // Output: [[-1, -1, 2], [-1, 0, 1]]
